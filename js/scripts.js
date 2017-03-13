47123dbc842f71428e4e904cd55ce50a1549e8ae
Alvaro Flores <alvaro.flores01@gmail.com>
2017-03-13 13:27:18 -0700

    adds more backend work

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 function add(number1, number2) {
   var add = number1 + number2;
-  console.log(add);
 }
 function subtract(number1, number2) {
   var subtract = number1 - number2;
-  console.log(subtract);
 }
 function multiply(number1, number2) {
   var multiply = number1 * number2;
-  console.log(multiply);
 }
 var sum = add(3, 4);
 var differnce = subtract(3, 4);
 var product = multiply(3, 4);
+
+var keys = document.querySelectorAll('.calculator span');
+var operators = ['+', '-', 'x', '/'];
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].onclick = function(e) {
+    var input = document.querySelector('#display');
+    var inputVal = input.innerHTML;
+    var btnVal = this.innerHTML;
+  }
+}
