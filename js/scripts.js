6f817b50265df66b5e47038b78211f91fe56ee37
Alvaro Flores <alvaro.flores01@gmail.com>
2017-03-13 11:52:11 -0700

    adds addition backend

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function add(number1, number2) {
+  var add = number1 + number2;
+  console.log(add);
+}
+function subtract(number1, number2) {
+  var subtract = number1 - number2;
+  console.log(subtract);
+}
+function multiply(number1, number2) {
+  var multiply = number1 * number2;
+  console.log(multiply);
+}
+var sum = add(3, 4);
+var differnce = subtract(3, 4);
+var product = multiply(3, 4);
