9415beb89bc93a1cd39119a3f15418ff708319df
Alvaro Flores <alvaro.flores01@gmail.com>
2017-03-13 17:13:00 -0700

    adds clear function

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,17 @@ function subtract(number1, number2) {
 function multiply(number1, number2) {
   var multiply = number1 * number2;
 }
-var sum = add(3, 4);
-var differnce = subtract(3, 4);
-var product = multiply(3, 4);
+var sum = add();
+var differnce = subtract();
+var product = multiply();
 
-var keys = document.querySelectorAll('.calculator span');
-var operators = ['+', '-', 'x', '/'];
-
-for (var i = 0; i < keys.length; i++) {
-  keys[i].onclick = function(e) {
-    var input = document.querySelector('#display');
-    var inputVal = input.innerHTML;
-    var btnVal = this.innerHTML;
-  }
-}
+ $(document).ready(function(){
+   $('.number').click(function(){
+    var key = $(this).val();
+    console.log(key);
+    $('#display').append(key);
+    });
+     $('.clear').click(function(){
+       $('#display').empty();
+  });
+});
